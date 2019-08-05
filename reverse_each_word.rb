2ccfa08a7c1589da4705040708c5bb3b9337e3f6
Finn Thye <piqueen@gmail.com>
2019-08-05 01:30:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(ordered_string)
   ordered_array = ordered_string.split(" ")
   final = ordered_array.each { |word| word.reverse }
-  puts final.class
+  puts final
 end
 
 reverse_each_word("Brian is my baby forever")
