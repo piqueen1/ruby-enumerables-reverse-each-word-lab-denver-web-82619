300c5b421e79b7b7756f8bede1d3eb65926abce0
Finn Thye <piqueen@gmail.com>
2019-08-05 01:38:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,14 @@
 def reverse_each_word(ordered_string)
   ordered_array = ordered_string.split(" ")
+  reversed_array = []
 
   final = ordered_array.each do |word|
     letter_array = word.split("")
     reversed_word = letter_array.reverse.join("")
-    puts reversed_word
+    reversed_array << reversed_word
   end
+  
+  reversed_array.join(" ")
 
 end
 
